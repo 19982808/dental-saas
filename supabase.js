95a740a567211e0f103d61e8ff7f6bbfbc1b58c6
Mercy Wambui <56608301+19982808@users.noreply.github.com>
2026-04-26 10:01:07 +0300

    Refactor supabase client initialization

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-export const supabase = createClient(
-  "https://gzoodfxfpztfmbybxina.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6b29kZnhmcHp0Zm1ieWJ4aW5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxMzU0MTcsImV4cCI6MjA5MjcxMTQxN30.K_5Flc8AnnnmYsitUgks11nl05ibqgH6rwqEfIL64_w"
-);
+const supabaseUrl = "https://gzoodfxfpztfmbybxina.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6b29kZnhmcHp0Zm1ieWJ4aW5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxMzU0MTcsImV4cCI6MjA5MjcxMTQxN30.K_5Flc8AnnnmYsitUgks11nl05ibqgH6rwqEfIL64_w";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
